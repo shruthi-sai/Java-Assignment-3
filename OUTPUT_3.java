3a64f3a294ef5afd4c4a98ee941ee3c9793a1012
shruthi-sai <89636224+shruthi-sai@users.noreply.github.com>
2021-09-19 18:43:55 +0530

    OUTPUT_3.java

---
+++ OUTPUT_3.java
@@ -0,0 +1,24 @@
+
+class Kcg{
+
+int i=0;
+
+Kcg(){
+
+i++;
+
+System.out.println(i);
+
+}
+
+public static void main(String b[]){
+
+Kcg d=new Kcg();
+
+Kcg d1=new Kcg();
+
+Kcg d2=new Kcg();
+
+}
+
+}
